ed32f0b06f0093002738e552d6d29f304de00ce1
juan.arboleda <juan.arboleda@pragma.com.co>
2017-03-29 10:30:11 -0500

    varios correos

--- src/enviarMail/enviar.java
+++ src/enviarMail/enviar.java
@@ -1,11 +1,14 @@
 package enviarMail;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.DataBufferInt;
 import java.awt.image.ImageObserver;
+import java.awt.image.WritableRaster;
 import java.io.*;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Connection;
@@ -13,14 +16,19 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Properties;
 
 
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
 import javax.imageio.ImageIO;
+import javax.mail.Address;
 import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.Session;
@@ -36,21 +44,22 @@ import javazoom.jl.player.Player;
 
 public class enviar{
 
-	public static void main(String[] args) throws IOException, SQLException, JavaLayerException
+	public static void main(String[] args) throws IOException, SQLException, JavaLayerException, ParseException
     {
 		//enviarMail();
 		//crearArchivo();
 		//pegarDB();
 		//sonidos();
-		File file1=new File("C:\\evidencias_nequi\\dos.jpg");
-		File file2=new File("C:\\evidencias_nequi\\tres.jpg");
+		//File file1=new File("C:\\comparar_nequi\\punto.jpg");
+		//File file2=new File("C:\\comparar_nequi\\punto2.jpg");
 		//File file1=new File("C:\\Users\\juan.arboleda\\Pictures\\81440.jpg");
 		//File file2=new File("C:\\Users\\juan.arboleda\\Pictures\\dos.png");
-		if(IsPngEquals(file1, file2)){
+		/*if(IsPngEquals(file1, file2)){
 			System.out.println("igual");
 		}else{
 			System.out.println("diferente");
-		}
+		}*/
+		enviarMailVarios();
     }
 	
 	public static void enviarMail(){
@@ -81,6 +90,54 @@ public class enviar{
 	            message.addRecipient(
 	                Message.RecipientType.TO,
 	                new InternetAddress("juan.arboleda@pragma.com.co"));
+	            message.setSubject("Pruebas");
+	            message.setContent(multiParte);
+
+	            Transport t = session.getTransport("smtp");
+	            t.connect("automat.sqa@gmail.com", "Sq@2017auto");
+	            t.sendMessage(message, message.getAllRecipients());
+	            t.close();
+	        }
+	        catch (Exception e)
+	        {
+	            e.printStackTrace();
+	        }
+	}
+	
+	public static void enviarMailVarios(){
+		 try
+	        {
+			 	String[] destinatarios = new String[3];
+			 	destinatarios[0]="juan.arboleda@pragma.com.co";
+			 	destinatarios[1]="juanc.arboleda@udea.edu.com";
+			 	destinatarios[2]="christian.fontalvo@sqasa.com";
+	            Properties props = new Properties();
+	            props.put("mail.smtp.host", "smtp.gmail.com");
+	            props.setProperty("mail.smtp.starttls.enable", "true");
+	            props.setProperty("mail.smtp.port", "587");
+	            props.setProperty("mail.smtp.user", "chuidiang@gmail.com");
+	            props.setProperty("mail.smtp.auth", "true");
+
+	            Session session = Session.getDefaultInstance(props, null);
+	            BodyPart texto = new MimeBodyPart();
+	            texto.setText("Texto del mensaje");
+
+	            BodyPart adjunto = new MimeBodyPart();
+	            adjunto.setDataHandler(
+	                new DataHandler(new FileDataSource("C:/Users/juan.arboleda/Pictures/39268.png")));
+	            adjunto.setFileName("39268.png");
+
+	            MimeMultipart multiParte = new MimeMultipart();
+	            multiParte.addBodyPart(texto);
+	            multiParte.addBodyPart(adjunto);
+
+	            MimeMessage message = new MimeMessage(session);
+	            //message.setFrom(new InternetAddress("juan.arboleda@pragma.com.co"));
+	            Address[] destinos = new Address[destinatarios.length];
+	            for(int i=0;i<destinos.length;i++){
+	                destinos[i]=new InternetAddress(destinatarios[i]);
+	            }
+	            message.addRecipients(Message.RecipientType.TO,destinos);
 	            message.setSubject("Pruebas");
 	            message.setContent(multiParte);
 
@@ -130,7 +187,7 @@ public class enviar{
 	      apl.play();     
 	}
 	
-	public static boolean IsPngEquals(File pngFile, File file) throws IOException {	
+	/*public static boolean IsPngEquals(File pngFile, File file) throws IOException {	
 		String Str_Ruta_Evidencias = "C:\\evidencias_nequi\\";
 		
 		BufferedImage imageA = ImageIO.read(pngFile);
@@ -141,13 +198,13 @@ public class enviar{
 	    
 	    BufferedImage recorte = ((BufferedImage) imageA).getSubimage(0,100,imageA.getWidth(),imageA.getHeight()-100);
 	    System.out.println(recorte);
-	    File img1 = new File("C:\\evidencias_nequi\\punto.jpg");
+	    File img1 = new File("C:\\comparar_nequi\\punto.jpg");
 	    ImageIO.write(recorte, "jpg", img1);
 	    BufferedImage recorte2 = ((BufferedImage) imageB).getSubimage(0,100,imageB.getWidth(),imageB.getHeight()-100);
-	    File img2 = new File("C:\\evidencias_nequi\\punto2.jpg");
+	    File img2 = new File("C:\\comparar_nequi\\punto2.jpg");
 	    ImageIO.write(recorte2, "jpg", img2);
-	    File file1=new File("C:\\evidencias_nequi\\punto.jpg");
-		File file2=new File("C:\\evidencias_nequi\\punto2.jpg");
+	    File file1=new File("C:\\comparar_nequi\\punto.jpg");
+		File file2=new File("C:\\comparar_nequi\\punto2.jpg");
 		BufferedImage imageC = ImageIO.read(file1);
 	    BufferedImage imageD = ImageIO.read(file2);
  
@@ -155,11 +212,35 @@ public class enviar{
 //	    DataBufferByte dataBufferB = (DataBufferByte)recorte2.getRaster().getDataBuffer();
 	    DataBufferInt dataBufferA = (DataBufferInt) imageC.getRaster().getDataBuffer();
 	    DataBufferInt dataBufferB = (DataBufferInt) imageD.getRaster().getDataBuffer();
+	    //WritableRaster raster = imageC.getRaster();
+	    //DataBufferByte data   = (DataBufferByte) raster.getDataBuffer();
 
-	    System.out.println(dataBufferA);
-	    System.out.println(dataBufferB);
+	    //System.out.println("a    "+data);
+	   // System.out.println(dataBufferB.getData());
 	    System.out.println(dataBufferA.getNumBanks());
 	    System.out.println(dataBufferB.getNumBanks());
+	    
+	    
+	    
+	    
+	    
+	    BufferedImage bi=ImageIO.read(new File("C:\\comparar_nequi\\punto.jpg"));
+	    int[] pixel;
+
+	    for (int y = 0; y < bi.getHeight(); y++) {
+	        for (int x = 0; x < bi.getWidth(); x++) {
+	            pixel = bi.getRaster().getPixel(x, y, new int[100]);
+	            Color c = new Color(pixel);
+	            System.out.println(pixel[0] + " - " + pixel[1] + " - " + pixel[2] + " - " + (bi.getWidth() * y + x));
+	        }
+	    }
+	    
+	    
+	    
+	    
+	    
+	    
+	    
  
 	    if (dataBufferA.getNumBanks() != dataBufferB.getNumBanks()) {
 	    	System.out.println(dataBufferA.getNumBanks());
@@ -178,7 +259,6 @@ public class enviar{
 	    }
  
 	    return true;
-	}
+	}*/
 	
-
 }
